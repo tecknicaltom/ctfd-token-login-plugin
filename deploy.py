a241d8e8951337a96a470a3aa60c2cece6457f11
Tom Samstag <github@modtwo.com>
2019-02-18 11:22:52 -0800

    add a deploy script

---
+++ deploy.py
@@ -0,0 +1,72 @@
+from itsdangerous import URLSafeTimedSerializer
+import glob
+import os
+import os.path
+import re
+import requests
+import tarfile
+import yaml
+
+secret = os.getenv('SECRET_KEY')
+if not secret:
+    raise ValueError('Environment variable SECRET_KEY not found')
+
+user = os.getenv('DEPLOYMENT_USER')
+if not user:
+    raise ValueError('Environment variable DEPLOYMENT_USER not found')
+
+ctfd_domain = os.getenv('CTFD_DOMAIN')
+if not ctfd_domain:
+    raise ValueError('Environment variable CTFD_DOMAIN not found')
+
+scoreboard_url_root = ctfd_domain
+if not scoreboard_url_root.startswith('http'):
+    scoreboard_url_root = 'https://' + ctfd_domain
+
+serializer = URLSafeTimedSerializer(secret)
+token = serializer.dumps(user)
+
+s = requests.Session()
+resp = s.post(scoreboard_url_root + '/api/token-login', json={'token': token})
+resp.raise_for_status()
+
+resp = s.get(scoreboard_url_root + '/admin/transfer')
+resp.raise_for_status()
+match = re.search(r'var csrf_nonce = "(?P<nonce>[a-f0-9]+)"', resp.text)
+if not match:
+    raise ValueError('CSRF nonce not found')
+nonce = match.group('nonce')
+print("extracted CSRF nonce: {}".format(nonce))
+
+for challenge_yaml in glob.glob('*/challenge.yaml'):
+    try:
+        print("Adding challenge yaml: {}".format(challenge_yaml))
+        additional_files = []
+        with open(challenge_yaml) as f:
+            challenge_data = yaml.load(f.read())
+
+            base = os.path.dirname(challenge_yaml)
+            for filename in challenge_data.get('files', []):
+                full_filename = os.path.join(base, filename)
+                additional_files.append((full_filename, filename))
+            
+        with open('export.yaml', 'w') as f:
+            f.write(yaml.dump_all([challenge_data]))
+
+        with tarfile.open('export.tar.gz', "w:gz") as tar:
+            tar.add('export.yaml')
+            for name, arcname in additional_files:
+                tar.add(name, arcname=arcname)
+
+        files = {'file': (
+            'export.tar.gz',
+            open('export.tar.gz', 'rb'),
+            'application/x-gzip'
+            )
+        }
+        resp = s.post(scoreboard_url_root + '/admin/yaml', files=files, data={'nonce': nonce})
+        resp.raise_for_status()
+        print("Success")
+    except Exception as e:
+        print("Exception thrown:")
+        print(e)
